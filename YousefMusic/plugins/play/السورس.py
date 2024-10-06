2b2a47c50ef9db71ca221192b89f35902c322583
HMD7SLK <sajd.haka11@gmail.com>
2024-10-06 18:10:49 +0200

    Update السورس.py

--- YousefMusic/plugins/play/السورس.py
+++ YousefMusic/plugins/play/السورس.py
@@ -12,23 +12,23 @@ from YousefMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, a
 from YousefMusic import app
 from random import  choice, randint
 
-# 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 ⛥ 𓏺 Yousef .tele_https://t.me/y_o_v
+# 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 ⛥ 𓏺 H M D .tele_https://t.me/xs_ub
                 
 @app.on_message(
     command(["سورس","• السورس •"," ","السورس"])
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://forkgraph.zaid.pro/file/mZMkbE56phEY",
-        caption = f"""<b>  <b>\n<a href="https://t.me/cecrr"> ➮ 𝐰𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐒𝐨𝐮𝐫𝐜𝐞 </a></b>""",
+        photo=f"https://i.ibb.co/yFvyj7C/aw-IRUSRibv-K.jpg",
+        caption = f"""<b>  <b>\n<a href="https://t.me/L_K_VV"> ➮ 𝐰𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐒𝐨𝐮𝐫𝐜𝐞 </a></b>""",
 reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                         "𝐃𝐞𝐯 𝐒𝐨𝐮𝐫𝐜𝐞", url=f"https://t.me/y_o_v"), 
+                         "𝐃𝐞𝐯 𝐒𝐨𝐮𝐫𝐜𝐞", url=f"https://t.me/xs_ub"), 
                  ],[
                    InlineKeyboardButton(
-                        "𝐒𝐨𝐮𝐫𝐜𝐞 ", url=f"https://t.me/cecrr"),
+                        "𝐒𝐨𝐮𝐫𝐜𝐞 ", url=f"https://t.me/L_K_VV"),
                 ],
 
             ]
