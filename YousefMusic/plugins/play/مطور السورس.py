f3f9fa06273f51f2adb87f1ba801983860ac8ae5
HMD7SLK <sajd.haka11@gmail.com>
2024-10-06 18:07:07 +0200

    Update مطور السورس.py

--- YousefMusic/plugins/play/مطور السورس.py
+++ YousefMusic/plugins/play/مطور السورس.py
@@ -34,15 +34,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OWNER_ID = 6094238403
+OWNER_ID = 7510088550
 OWNER = getenv("OWNER")
 
 
 
-@app.on_message(filters.command(["مطور السورس", "• مطور السورس •","يوسف"], ""))
+@app.on_message(filters.command(["مطور السورس", "• مطور السورس •","حمد"], ""))
 async def dev(client: Client, message: Message):
      bot_username = client.me.username
-     user = await client.get_chat(6094238403)
+     user = await client.get_chat(7510088550)
      name = user.first_name
      username = user.username 
      bio = user.bio
