2b9c6e712e3b0dd3916d59ec7b6784132d24049b
BASIL8WB <sajd.hapa82@gmail.com>
2024-10-08 00:03:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/BASIL8WB/Source_Basil",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "BASl")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Yousef")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
